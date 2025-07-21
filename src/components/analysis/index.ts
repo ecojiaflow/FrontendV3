e45ee45e10ee08d9b6f7c2a6374eb3185174aad1
Salim Benamara <salim.d.benamara@gmail.com>
2025-07-21 17:19:32 +0200

    Réinclut src/ pour corriger AuthContext invisible sur Netlify

--- src/components/analysis/index.ts
+++
@@ -1,5 +0,0 @@
-export { CosmeticsAnalyzer, DetergentsAnalyzer } from './CosmeticsAnalyzer';
-export { CosmeticAnalysisDisplay, DetergentAnalysisDisplay } from './CosmeticAnalysisDisplay';
-export { default as AlternativesSuggestions } from './AlternativesSuggestions';
-export { default as NovaAlert } from './NovaAlert';
-export { default as ScientificScore } from './ScientificScore';
